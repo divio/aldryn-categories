6e4fe96a7f3681b5a9725ba4ed8a19f65a852327
Martin Koistinen <mkoistinen@gmail.com>
2015-01-07 18:23:07 +0200

    Add admin [WIP]

--- aldryn_categories/admin.py
+++ aldryn_categories/admin.py
@@ -2,13 +2,25 @@
 
 from django.contrib import admin
 
+from parler.admin import TranslatableAdmin
+
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
 from .models import Category
 
 
-class CategoryAdmin(TreeAdmin):
+class CategoryAdmin(TranslatableAdmin, TreeAdmin):
     form = movenodeform_factory(Category)
+    list_display = ('name', 'slug')
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+            ),
+        }),
+    )
+
 
 admin.site.register(Category, CategoryAdmin)
