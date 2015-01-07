250a87b932d00d7523fcd3e08bd6ffd31a0289e6
Martin Koistinen <mkoistinen@gmail.com>
2015-01-07 18:22:59 +0200

    Remove Abstract Base Class

--- aldryn_categories/models.py
+++ aldryn_categories/models.py
@@ -15,13 +15,20 @@ from parler.models import TranslatableModel, TranslatedFields
 from treebeard.ns_tree import NS_Node
 
 
+#
+# NOTE: I would have preferred to make this a base class "CategoryBase" which
+# is Abstract, then subclass it as a concreate Category class. But, Parler
+# cannot be applied to an Abstract class.
+#
+# TODO: At some point, consider an approach like this:
+#     https://gist.github.com/GaretJax/7c7a9acc055c05c65041
+#
 class Category(TranslatableModel, NS_Node):
     """
     A category is hierarchical. The structure is implemented with django-
     treebeard's Nested Sets trees, which has the performance characteristics
     we're after, namely: fast reads at the expense of write-speed.
     """
-
     node_order_by = ['name', ]
 
     translations = TranslatedFields(
@@ -33,13 +40,13 @@ class Category(TranslatableModel, NS_Node):
         ),
         slug=models.SlugField(
             _('slug'),
-            help_text=_('Provide a “slug” or leave blank for an automatically generated one.'),
+            help_text=_('Provide a “slug” or leave blank for an automatically '
+                        'generated one.'),
             max_length=255,
         )
     )
 
     class Meta:
-        # Sadly, an Abstract class cannot also be translatable.
         verbose_name = _('category')
         verbose_name_plural = _('categories')
 
