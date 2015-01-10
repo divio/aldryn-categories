0c209806bae1b0dc36d265e906b038f699570c04
Martin Koistinen <mkoistinen@gmail.com>
2015-01-10 15:55:32 +0200

    Add custom ModelField with custom default FormField
    Includes special consideration for South

--- README.md
+++ README.md
@@ -12,6 +12,32 @@
 ### Dependencies/Support
 
 1. Python v2.7+
-2. Django v1.6+
+2. Django v1.6.x + South v1.0.2 or Django v1.7+
 3. django-treebeard v2.0+
 4. django-parler v1.2.1+
+
+
+## Usage
+
+To use Aldryn Categories, install the package (currently, only available from
+its GitHub repository) with:
+
+	`pip install https://github.com/aldryn/aldryn-categories/archive/master.zip`
+
+Next, add `aldryn_categories` to your `INSTALLED_APPS`.
+
+Now, add a `CategoryManyToManyField` to `aldryn_categories.Category` on any
+models you wish to categorize, like so:
+
+	# -*- coding: utf-8 -*-
+
+	from django.db import models
+	from aldryn_categories.fields import CategoryManyToManyField
+
+	class Thing(models.Model):
+	    my_field = models.CharField(...)
+	    ...
+	    categories = CategoryManyToManyField('aldryn_categories.Category')
+
+This usage of the CategoryManyToManyField simply allows your categories to be
+displayed heirarchically in the otherwise normal MultipleSelectWidget.
---
+++ aldryn_categories/fields.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import warnings
+
+from django.db.models.fields.related import ManyToManyField
+from django.forms.models import ModelMultipleChoiceField
+from django.utils.safestring import mark_safe
+
+# For South, where used.
+try:
+    from south.modelsinspector import add_introspection_rules
+except:
+    add_introspection_rules = False
+
+
+class CategoryMultipleChoiceField(ModelMultipleChoiceField):
+    """Displays the choices heirarchically as per their position in the tree.
+    """
+    def label_from_instance(self, obj):
+        prefix = ''
+        try:
+            if obj.depth > 1:
+                prefix = '&nbsp;&nbsp;' * (obj.depth - 1)
+        except:
+            warnings.warn(
+                "CategoryMultipleChoiceField should only be used for M2M "
+                "relations to the aldryn_categories.Category model.")
+
+        return mark_safe("{prefix}{name}".format(prefix=prefix, name=obj.name))
+
+
+class CategoryManyToManyField(ManyToManyField):
+    """Simply a normal ManyToManyField, but with a custom *default* form field
+    which has a heirarchically displayed set of choices.
+    """
+    def formfield(self, form_class=CategoryMultipleChoiceField, choices_form_class=None, **kwargs):
+        kwargs["form_class"] = form_class
+        kwargs["choices_form_class"] = choices_form_class
+        return super(CategoryManyToManyField, self).formfield(**kwargs)
+
+
+if add_introspection_rules:
+    add_introspection_rules([], ["^aldryn_categories\.fields\.CategoryManyToManyField"])
