7d3cf6f818fd65c49d3c8d531c6cb9e61ef68786
Martin Koistinen <mkoistinen@gmail.com>
2015-01-13 17:25:03 +0200

    Switch to djangocms-helper from django-app-manage

---
+++ test_settings.py
@@ -0,0 +1,13 @@
+HELPER_SETTINGS = {
+    'TIME_ZONE': 'Europe/Zurich',
+    'LANGUAGES': (
+        ('en', 'English'),
+        ('de', 'German'),
+        ('de', 'French'),
+    ),
+    'INSTALLED_APPS': [
+        'parler',
+        'treebeard',
+        'aldryn_categories',
+    ],
+}
