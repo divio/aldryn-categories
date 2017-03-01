4aacb0985c29418a22789917c99bc332219a3278
Paulo <commonzenpython@gmail.com>
2017-03-01 13:46:59 -0500

    Updated translations

--- aldryn_categories/admin.py
+++ aldryn_categories/admin.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-
 from django.contrib import admin
+from django.utils.translation import ugettext
 
 from parler.admin import TranslatableAdmin
 
 from treebeard.admin import TreeAdmin
-from treebeard.forms import movenodeform_factory, MoveNodeForm
 
-from parler.forms import TranslatableModelForm
-
+from .forms import CategoryAdminForm
 from .models import Category
 
 
-class CategoryForm(TranslatableModelForm, MoveNodeForm):
-    pass
-
-
 class CategoryAdmin(TranslatableAdmin, TreeAdmin):
-    form = movenodeform_factory(Category, form=CategoryForm)
+    form = CategoryAdminForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        FormClass = super(CategoryAdmin, self).get_form(request, obj, **kwargs)
+        # Workaround for missing translations on treebeard
+        FormClass.base_fields['_position'].label = ugettext('Position')
+        FormClass.base_fields['_ref_node_id'].label = ugettext('Relative to')
+        return FormClass
+
 
 admin.site.register(Category, CategoryAdmin)
